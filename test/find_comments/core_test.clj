79eb7efe4f751222b83784ce35c4558c75329354
Aleksandr Sher <aleksandrsher@gmail.com>
2016-05-24 15:56:38 +0300

    move to expectations

--- test/find_comments/core_test.clj
+++ test/find_comments/core_test.clj
@@ -1,65 +1,80 @@
 (ns find-comments.core-test
-  (:use midje.sweet
-        find-comments.core))
+  (:require [expectations :refer [expect from-each]])
+  (:use find-comments.core))
 
-(facts "canary tests"
-  (fact "truthiness"
-    true => true)
-  
-  (fact "falsiness"
-  false => false))
+;; helpers
 
-(facts "about finding comment in line"
-  (let [fc (partial find-comment-in-line ["#" "//"])]
-    (fact "there is no comment in line"
-      (fc "") => nil
-      (fc "test") => nil
-      (fc "some $$ strange && symbols") => nil
-      (fc "split('//');") => nil
-      (fc "/1/2/3/4") => nil)
-    (fact "there is some comment in line"
-      (fc "#all string is comment!") => "all string is comment!"
-      ;; empty comment is still a comment ;)
-      (fc "#") => ""
-      (fc "'a#' # test") => " test"
-      (fc "'a'# # hooray") => " # hooray"
-      (fc "'a\"//' # test # some") => " test # some"
-      (fc "//  ") => "  "
-      (fc "// first # second") => " first # second"
-      (fc "# first // second") => " first // second"
-      )))
+(defmacro expect-cases
+  [cases f]
+  (cons 'do
+        (for [[data expected] (eval cases)]
+          `(expect ~expected (~f ~data)))))
 
-(facts "about finding comments"
-  (fact "multiple comments in a file"
-    (find-comments-in-file "./dev-resources/sample_code/file1.php") => '(
-      "comment (single line)" 
-      " lagun !! naiz hizkuntza bat ez dut ulertzen in Iruzkin bat harrapatuta !! lagundu nazakezu?"
-      " another comment"))
+;; canary tests
 
-  (fact "comments can contain comment tokens"
-    (find-comments-in-file "./dev-resources/inception/movie.php") => '(
-      "a comment"
-      "another comment"
-      "a // within a #"
-      "we need to go deeper"
-      "i heard // you like #s so i put a // in your # so you can // while you #")))
+(expect true true)
 
-(facts "about finding php files"
+;; about finding comment in line
 
-  (fact "a single level"
-    (find-php-files "./dev-resources/sample_code/") => '("./dev-resources/sample_code/file1.php"))
+;; - there is no comment in line:
 
-  (fact "no need to specify the slash at the end of the path"
-    (find-php-files "./dev-resources/sample_code") => '("./dev-resources/sample_code/file1.php"))
+(def nil-cases ["" 
+                "test" 
+                "some $$ strange && symbols"
+                "split('//');"
+                "/1/2/3/4"])
 
-  (fact "multiple levels"
-    (find-php-files "./dev-resources/matching_at_multiple_levels/") => '(
-      "./dev-resources/matching_at_multiple_levels/file1.php"
-      "./dev-resources/matching_at_multiple_levels/folder/file2.php"))
+(expect nil (from-each [l nil-cases] (find-comment-in-line l)))
 
-  (fact "only matches php files, not folders"
-    (find-php-files "./dev-resources/folder_matching_pattern/") => '())
+;; - there is some comment in line:
 
-  (fact "only matches files with php extension"
-    (find-php-files "./dev-resources/bad_extension/") => '()))
+(def comment-cases
+  {"#all string" "all string" 
+   "#" ""
+   "'a#' # test" " test"
+   "'a'# # hooray" " # hooray"
+   "'a\"//' # test # some" " test # some"
+   "//  " "  "
+   "// first # second" " first # second"
+   "# first // second" " first // second"})
+
+(expect-cases comment-cases find-comment-in-line)
+
+;; about finding comments
+
+(def file-cases
+  {;; - multiple comments in a file
+   "./dev-resources/sample_code/file1.php" 
+   ["comment (single line)" 
+    " lagun !! naiz hizkuntza bat ez dut ulertzen in Iruzkin bat harrapatuta !! lagundu nazakezu?"
+    " another comment"]
+   ;; - comments can contain comment tokens
+   "./dev-resources/inception/movie.php"
+   ["a comment" 
+    "another comment" 
+    "a // within a #" 
+    "we need to go deeper" 
+    "i heard // you like #s so i put a // in your # so you can // while you #"]})
+
+(expect-cases file-cases find-comments-in-file)
+
+;; about finding php files
+
+;; - a single level
+(expect '("./dev-resources/sample_code/file1.php") (find-php-files "./dev-resources/sample_code/"))
+
+;; - no need to specify the slash at the end of the path
+(expect '("./dev-resources/sample_code/file1.php") (find-php-files "./dev-resources/sample_code"))
+
+;; - multiple levels
+(expect 
+ ["./dev-resources/matching_at_multiple_levels/file1.php"
+  "./dev-resources/matching_at_multiple_levels/folder/file2.php"]
+  (find-php-files "./dev-resources/matching_at_multiple_levels/"))
+
+;; - only matches php files, not folders
+(expect [] (find-php-files "./dev-resources/folder_matching_pattern/"))
+
+;; - only matches files with php extension
+(expect [] (find-php-files "./dev-resources/bad_extension/"))
 
